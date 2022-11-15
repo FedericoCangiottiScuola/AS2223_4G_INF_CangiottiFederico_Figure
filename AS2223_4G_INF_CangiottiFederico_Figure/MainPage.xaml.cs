c507dc0be3cef61bea4258b6e03579ddd49c7cb0
Federico Cangiotti <federico.cangiotti05@gmail.com>
2022-11-15 18:18:12 +0100

    feat: Aggiunge il cambio dinamico dell'interfaccia

--- AS2223_4G_INF_CangiottiFederico_Figure/MainPage.xaml.cs
+++ AS2223_4G_INF_CangiottiFederico_Figure/MainPage.xaml.cs
@@ -6,4 +6,44 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void cmbFigure_SelectedIndexChanged(object sender, EventArgs e)
+    {
+		switch (cmbFigure.SelectedItem)
+		{
+			case "quadrato":
+                lblBase.IsVisible= false;
+                txtBase.IsVisible= false;
+                lblAltezza.IsVisible= false;
+                txtAltezza.IsVisible = false;
+                lblRaggio.IsVisible= false;
+                txtRaggio.IsVisible = false;
+
+                lblLato.IsVisible = true;
+                txtLato.IsVisible = true;
+                break;
+            case "rettangolo":
+                lblLato.IsVisible = false;
+                txtLato.IsVisible = false;
+                lblRaggio.IsVisible = false;
+                txtRaggio.IsVisible = false;
+
+                lblBase.IsVisible = true;
+                txtBase.IsVisible = true;
+                lblAltezza.IsVisible = true;
+                txtAltezza.IsVisible = true;
+                break;
+            case "cerchio":
+                lblLato.IsVisible = false;
+                txtLato.IsVisible = false;
+                lblBase.IsVisible = false;
+                txtBase.IsVisible = false;
+                lblAltezza.IsVisible = false;
+                txtAltezza.IsVisible = false;
+
+                lblRaggio.IsVisible = true;
+                txtRaggio.IsVisible = true;
+                break;
+        }
+    }
 }
