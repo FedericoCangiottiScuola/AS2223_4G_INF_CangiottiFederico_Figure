9a94f8992e34b5e4dc31c26838ff528b053a1376
Federico Cangiotti <federico.cangiotti05@gmail.com>
2022-11-16 15:42:01 +0100

    feat: Aggiunge la classe Rettangolo

---
+++ AS2223_4G_INF_CangiottiFederico_Figure/Rettangolo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS2223_4G_INF_CangiottiFederico_Figure
+{
+    internal class Rettangolo : Figura
+    {
+        double b;
+        double altezza;
+
+        public Rettangolo(double b, double altezza)
+        {
+            this.b = b;
+            this.altezza = altezza;
+            nome = "Rettangolo";
+            lati = 4;
+        }
+
+        public override double Perimetro()
+        {
+            return b * 2 + altezza * 2;
+        }
+
+        public override double Area()
+        {
+            return b * altezza;
+        }
+    }
+}
