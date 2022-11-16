76c408c77713dddc782a0dc918af3a4427389238
Federico Cangiotti <federico.cangiotti05@gmail.com>
2022-11-16 15:42:47 +0100

    feat: Aggiunge la classe Cerchio

---
+++ AS2223_4G_INF_CangiottiFederico_Figure/Cerchio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS2223_4G_INF_CangiottiFederico_Figure
+{
+    internal class Cerchio : Figura
+    {
+        double raggio;
+
+        public Cerchio(double raggio)
+        {
+            this.raggio = raggio;
+            nome = "Cerchio";
+        }
+
+        public override double Perimetro()
+        {
+            return 2 * Math.PI * raggio;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * raggio * raggio;
+        }
+    }
+}
