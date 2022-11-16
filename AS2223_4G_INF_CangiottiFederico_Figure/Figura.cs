59131262c33bc56f3b6e0503d5f9d213c8766bb6
Federico Cangiotti <federico.cangiotti05@gmail.com>
2022-11-16 15:39:40 +0100

    feat: Aggiunge la classe Figura

---
+++ AS2223_4G_INF_CangiottiFederico_Figure/Figura.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS2223_4G_INF_CangiottiFederico_Figure
+{
+    internal class Figura
+    {
+        protected string? nome;
+        protected int lati;
+
+        protected Figura()
+        {
+            nome = "Figura generica";
+            lati = 0;
+        }
+
+        public virtual double Perimetro()
+        {
+            return 0;
+        }
+
+        public virtual double Area()
+        {
+            return 0;
+        }
+    }
+}
