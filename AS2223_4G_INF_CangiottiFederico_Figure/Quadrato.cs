05c1d829b137bb1cf5af6f3f972026023ce27c3f
Federico Cangiotti <federico.cangiotti05@gmail.com>
2022-11-16 15:40:31 +0100

    feat: Aggiunge la classe Quadrato

---
+++ AS2223_4G_INF_CangiottiFederico_Figure/Quadrato.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS2223_4G_INF_CangiottiFederico_Figure
+{
+    internal class Quadrato : Figura
+    {
+        double lato;
+
+        public Quadrato(double lato)
+        {
+            this.lato = lato;
+            nome = "Quadrato";
+            lati = 4;
+        }
+
+        public override double Perimetro()
+        {
+            return lato * 4;
+        }
+
+        public override double Area()
+        {
+            return lato * lato;
+        }
+    }
+}
